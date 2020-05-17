6246944177b0cc35e76fb44d4a3aa59a01e6e349
Vineeta <49141391+Vineeta12345@users.noreply.github.com>
2020-05-17 18:10:41 +0530

    Add files via upload

---
+++ selectionsort.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 15:38:59 2020
+
+@author: Vineeta
+"""
+
+#swap two elements in nArr at postion i and j
+def swap(i, j, nArr):
+    if i!=j:     
+        temp = nArr[j]
+        nArr[j] = nArr[i]
+        nArr[i] = temp
+ 
+#function to sort elements in the list
+def selectionSort(nArr):    
+    for i in range(0, len(nArr)):
+        small = i
+        for j in range(i+1, len(nArr)):
+            if nArr[j] < nArr[small]:
+                small = j
+        swap(i, small, nArr)
+ 
+#list containing elements to sort        
+nArr = [34,456, 5, 5,67]
+selectionSort(nArr)
+print(nArr)
